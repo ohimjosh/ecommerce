31b5b7d5d827e422990f5dc15dbb1d1a04659dda
Josh <joshmiranda.exe@gmail.com>
2022-07-31 13:32:37 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Store from './pages/Store';
-import About from './pages/About';
+import "./App.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
+import NavbarComp from "./components/NavbarComp";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/" element={<Store />} />
-          <Route path="/" element={<About />} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <div>
+        <NavbarComp />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </Router>
   );
 }
 
